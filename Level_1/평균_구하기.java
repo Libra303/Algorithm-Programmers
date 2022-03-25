9d1507733f663768dbe4717b6ab1179b1f5571f1
Libra <jihyuk8778@naver.com>
2022-03-25 09:56:55 +0900

    Solving 평균_구하기

---
+++ Level_1/평균_구하기.java
@@ -0,0 +1,18 @@
+package Level_1;
+
+public class 평균_구하기 {
+	
+	class Solution {
+	    public double solution(int[] arr) {
+	    	
+	        double answer = 0;
+	        
+	        for(int i = 0; i < arr.length; i++) {
+	        	answer += arr[i];
+	        }
+	        
+	        return answer / arr.length;
+	    }
+	}
+
+}
